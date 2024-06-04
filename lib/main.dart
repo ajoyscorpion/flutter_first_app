0e5d232cf204cc56f3a398ea5d74761ee62d6eb1
Paulson Mathew <ajoyscorpion@gmail.com>
2024-06-04 23:31:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 123, 0),
+      appBar: AppBar(),
+    );
+  }
+}
+
+
